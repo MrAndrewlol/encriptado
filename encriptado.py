ba919a5678012f1c1d4a5c8a15170c2a591d270f
MrAndrewlol <40283828+MrAndrewlol@users.noreply.github.com>
2023-11-24 17:12:25 -0600

    done

--- encriptado.py
+++ encriptado.py
@@ -1,4 +1,10 @@
 
+##PROGRAMA REALIZADO POR
+
+#GUSTAVO CRUZ 22779
+#ANDRE JO 22199
+
+
 
 #INPUTS
 
@@ -7,6 +13,94 @@
 
 #Bloques de 4 digitos
 #Funciones
+
+# Ansi para impresión de pantalla con colores en terminal
+CLEAR = "\033[2J"
+BLUE = "\033[94m"
+RED = "\033[91m"
+GREEN = "\033[92m"
+RESET = "\033[0m"
+
+import time
+# Funcion que imprime lento
+def slowprint(text):
+    for char in text:
+        print(char, end ='', flush=True)
+        time.sleep(0.1)
+# Funcion para verificar si un numero es primo
+def is_prime(n):
+    if n <=1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+# Funcion para encontrar numeros primos de 2 a raiz cuadradada de n
+def get_primes(n0):
+    primes = []
+    for num in range(2, n0 +1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
+# Funcion que encuentra los numeros primos p y q
+def findpq(n):
+    primes = get_primes(n)
+    
+    for p in primes:
+        if n % p == 0:
+            q = n // p
+            return p,q
+    return None, None
+
+# Funcion que implementa el algoritmo extendido de euclides, retorna mcd, y coeficientes x, y
+def euclides(a,b):
+    if a == 0:
+        return(b, 0, 1)
+    else:
+        mcd, x, y = euclides(b%a, a)
+    return (mcd, y -(b//a) *x, x)
+
+# Funcion para calcular el modulo inverso multiplicativo
+def modinverse(a,m):
+    mcd, x, y = euclides(a,m)
+    if mcd != 1:
+        slowprint(f"{RED} ERROR... NO HAY INVERSO MULTIPLICATIVO ...")
+    else:
+        return x%m
+
+# Funcion para calcular la llave privada    
+def privKey(e, p, q):
+    phi = (p-1)*(q-1)
+    return modinverse(e, phi) 
+
+def decrypt(C, d, n):
+    M = ""
+    for i in range(0, len(C), 4):
+        bloque = C[i:i + 4]
+        num = int(bloque)
+        dec_num = pow(num, d, n)
+        letra1 = chr((dec_num // 100) + ord('A'))
+        letra2 = chr((dec_num % 100) + ord('A'))
+        M += letra1 + letra2
+    return M
+
+def main():
+    #slowprint(f"{CLEAR}\t\t{BLUE} Bienvenido al programa de codificación y decodificación RSA{RESET}\n\n")
+    C_in = int(input(f"{GREEN}Ingrese el mensaje cifrado, en bloques de dos letras: ").replace(" ", ""))
+    C = str(C_in)
+    e = int(input("Ingrese el valor de e: "))
+    n = int(input("Ingrese el valor de n: "))
+    print(CLEAR)
+    #slowprint(f"{BLUE}...Calculando p y q...")
+    p,q = findpq(n)    
+    #slowprint(f"\n{GREEN}Encontrado p:{p} q: {q}")
+    #slowprint(f"\n{BLUE}Calculando llave privada... {RESET}")
+    d = privKey(e,p,q)
+    #slowprint(f"\n{CLEAR}{GREEN}Llave privada d: {d}{RESET}")
+    M = decrypt(C,d,n)
+    print(f"\n\t{RED} El mensaje descifrado es: {M}")
+
 
 
 def prime(numero):
@@ -54,9 +148,6 @@ def Mconverter(lista):
 
             
 
-
-        
-
 #Funcion para poder saber si es primo o no
 
 
@@ -87,121 +178,133 @@ while(menu):
         
     print("Bienvenido al Encripación RSA")
 
-    Mensaje = input("Porfavor Escribir el mensaje que desea Encriptar\n")
-    Mensaje = Mensaje.upper()
-    Mensaje = Mensaje.replace(" ", "")
+    print("\n Por favor elegir las siguientes opciones")
+    print("\n1.Encriptación\n2.Desencriptación")
 
-    lista = list(Mensaje)
-    mensajemod = Mconverter(lista)
+    decision = input( "1 para encriptar y 2 para desencriptar\n")
 
-
-    numerop = int(input("Escibir su primer numero primo\n"))
-
-    numeroq = int(input("Escribir su segundo numero primo\n"))
-
-    if(prime(numerop) == False):
-        print("El primer numero no es primo")
-        parte2 = False  
-        
-    if(prime(numeroq) == False):
-        print("El Segundo numero no es primo")
-        parte2 = False
-    
-
-    while(parte2 == True):
-        print("Segunda parte...")
-        e = round(int(input("Por favor ingresar un numero entero y que sea primo relativo a " + str(numerop*numeroq) + "\n")))
-         ##Basicamente si p y q son menores a 2525 entonces da eso por ejemplo 3551 por lo tanto son de dos caracteres y ahi lo separas en 4 bloques
-        print()
-        if(relativeprime(e,(numerop*numeroq)) == 1): 
-            parte2 = False
-            print("Tercer parte....")
-            ##Esta parte vamos a codificar los bloques
+    if(decision == "1"):
             
+        Mensaje = input("Porfavor Escribir el mensaje que desea Encriptar\n")
+        Mensaje = Mensaje.upper()
+        Mensaje = Mensaje.replace(" ", "")
 
-            if((numerop*numeroq) > int(obtenernum(numerop*numeroq)) ): #3 Digitos
-                mensajencriptad = ""
-                numeroglobal = contador((numerop*numeroq))
-                if((len(lista) % int(numeroglobal)) == 0):
-                    print("No se necesita Dummy variable")
-                    b = 0
-                    mensaje = 0
-                    while(b <= ((len(lista)-1)/(int(numeroglobal)/2))):
-                        messegae1 = ""
-                        a = 0
-                        while(a < (int(numeroglobal)/2)):
-                            messegae1 = messegae1 + str(lista[mensaje + a])
-                            a = a+1
+        lista = list(Mensaje)
+        mensajemod = Mconverter(lista)
 
-                        #messegae1 = str(lista[mensaje]) + str(lista[mensaje+1]) + str(lista[mensaje+2]) 
-                        if(int(numeroglobal) <= 10):
-                            numeroglobal = "{:02}".format(numeroglobal)
 
-                        numberformat = "{:" + str(numeroglobal) +"}"
-                        messegae = int(int(messegae1)**e) 
-                        messegae = messegae % (numerop*numeroq)
-                        messegae = numberformat.format(messegae)
-                        menecript.append(messegae)
-                        mensaje = round(mensaje + (int(numeroglobal)/2))
-                        b = b+1
-                    
-                    for i in menecript:
-                        mensajencriptad = mensajencriptad + i
-                    resultado = [mensajencriptad[i:i+4] for i in range(0, len(mensajencriptad), 4)]
+        numerop = int(input("Escibir su primer numero primo\n"))
+
+        numeroq = int(input("Escribir su segundo numero primo\n"))
+
+        if(prime(numerop) == False):
+            print("El primer numero no es primo")
+            parte2 = False  
+            
+        if(prime(numeroq) == False):
+            print("El Segundo numero no es primo")
+            parte2 = False
+        
+
+        while(parte2 == True):
+            print("Segunda parte...")
+            e = round(int(input("Por favor ingresar un numero entero y que sea primo relativo a " + str(numerop*numeroq) + "\n")))
+            ##Basicamente si p y q son menores a 2525 entonces da eso por ejemplo 3551 por lo tanto son de dos caracteres y ahi lo separas en 4 bloques
+            print()
+            if(relativeprime(e,(numerop*numeroq)) == 1): 
+                parte2 = False
+                print("Tercer parte....")
+                ##Esta parte vamos a codificar los bloques
+                
+
+                if((numerop*numeroq) > int(obtenernum(numerop*numeroq)) ): #3 Digitos
                     mensajencriptad = ""
-                    for i in resultado:
-                        mensajencriptad = mensajencriptad + i + " "
+                    numeroglobal = contador((numerop*numeroq))
+                    if((len(lista) % int(numeroglobal)) == 0):
+                        print("No se necesita Dummy variable")
+                        b = 0
+                        mensaje = 0
+                        while(b <= ((len(lista)-1)/(int(numeroglobal)/2))):
+                            messegae1 = ""
+                            a = 0
+                            while(a < (int(numeroglobal)/2)):
+                                messegae1 = messegae1 + str(lista[mensaje + a])
+                                a = a+1
 
-                    print("El mensaje encriptado es: " +mensajencriptad)
-                    print("LLave publica es (" + str(e) + "," +  str(numerop*numeroq) + ")" )
-                    menu = False
+                            #messegae1 = str(lista[mensaje]) + str(lista[mensaje+1]) + str(lista[mensaje+2]) 
+                            if(int(numeroglobal) <= 10):
+                                numeroglobal = "{:02}".format(numeroglobal)
+
+                            numberformat = "{:" + str(numeroglobal) +"}"
+                            messegae = int(int(messegae1)**e) 
+                            messegae = messegae % (numerop*numeroq)
+                            messegae = numberformat.format(messegae)
+                            menecript.append(messegae)
+                            mensaje = round(mensaje + (int(numeroglobal)/2))
+                            b = b+1
+                        
+                        for i in menecript:
+                            mensajencriptad = mensajencriptad + i
+                        resultado = [mensajencriptad[i:i+4] for i in range(0, len(mensajencriptad), 4)]
+                        mensajencriptad = ""
+                        for i in resultado:
+                            mensajencriptad = mensajencriptad + i + " "
+
+                        print("El mensaje encriptado es: " +mensajencriptad)
+                        print("LLave publica es (" + str(e) + "," +  str(numerop*numeroq) + ")" )
+                        menu = False
+
+
+                        phi = (numerop-1)*(numeroq-1) #LLAVE QUE NO SE DEBE DE REVELAR
+                            
+                    else:
+                        print("Se necesita un Dummy")
+                        i = 0
+                        while(i <= (len(lista) % int(numeroglobal))):
+                            lista.append("00")
+                            i =+ 1
+                        b = 0
+                        mensaje = 0
+                        while(b <= ((len(lista)-1)/(int(numeroglobal)/2))):
+                            messegae1 = ""
+                            a = 0
+                            while(a < (int(numeroglobal)/2)):
+                                messegae1 = messegae1 + str(lista[mensaje + a])
+                                a = a+1
+
+                            #messegae1 = str(lista[mensaje]) + str(lista[mensaje+1]) + str(lista[mensaje+2]) 
+                            if(int(numeroglobal) <= 10):
+                                numeroglobal = "{:02}".format(numeroglobal)
+
+                            numberformat = "{:" + str(numeroglobal) +"}"
+                            messegae = int(int(messegae1)**e) 
+                            messegae = messegae % (numerop*numeroq)
+                            messegae = numberformat.format(messegae)
+                            menecript.append(messegae)
+                            mensaje = round(mensaje + (int(numeroglobal)/2))
+                            b = b+1
+                        
+                        for i in menecript:
+                            mensajencriptad = mensajencriptad + i
+                        resultado = [mensajencriptad[i:i+4] for i in range(0, len(mensajencriptad), 4)]
+                        mensajencriptad = ""
+                        for i in resultado:
+                            mensajencriptad = mensajencriptad + i + " "
+
+                        print("El mensaje encriptado es: " +mensajencriptad)
+                        print("LLave publica es (" + str(e) + "," +  str(numerop*numeroq) + ")" )
+                        menu = False
 
 
                     phi = (numerop-1)*(numeroq-1) #LLAVE QUE NO SE DEBE DE REVELAR
-                        
-                else:
-                    print("Se necesita un Dummy")
-                    i = 0
-                    while(i <= (len(lista) % int(numeroglobal))):
-                        lista.append("00")
-                        i =+ 1
-                    b = 0
-                    mensaje = 0
-                    while(b <= ((len(lista)-1)/(int(numeroglobal)/2))):
-                        messegae1 = ""
-                        a = 0
-                        while(a < (int(numeroglobal)/2)):
-                            messegae1 = messegae1 + str(lista[mensaje + a])
-                            a = a+1
+            else:
+                print("Numero ingresado no es primo relativo")
 
-                        #messegae1 = str(lista[mensaje]) + str(lista[mensaje+1]) + str(lista[mensaje+2]) 
-                        if(int(numeroglobal) <= 10):
-                            numeroglobal = "{:02}".format(numeroglobal)
-
-                        numberformat = "{:" + str(numeroglobal) +"}"
-                        messegae = int(int(messegae1)**e) 
-                        messegae = messegae % (numerop*numeroq)
-                        messegae = numberformat.format(messegae)
-                        menecript.append(messegae)
-                        mensaje = round(mensaje + (int(numeroglobal)/2))
-                        b = b+1
-                    
-                    for i in menecript:
-                        mensajencriptad = mensajencriptad + i
-                    resultado = [mensajencriptad[i:i+4] for i in range(0, len(mensajencriptad), 4)]
-                    mensajencriptad = ""
-                    for i in resultado:
-                        mensajencriptad = mensajencriptad + i + " "
-
-                    print("El mensaje encriptado es: " +mensajencriptad)
-                    print("LLave publica es (" + str(e) + "," +  str(numerop*numeroq) + ")" )
-                    menu = False
+    if(decision == "2"):
+        main()
+        menu = False
 
 
-                phi = (numerop-1)*(numeroq-1) #LLAVE QUE NO SE DEBE DE REVELAR
-        else:
-            print("Numero ingresado no es primo relativo")
-
-        
+            
 
 
